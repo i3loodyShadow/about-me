abd1ba607fa864267c09e86dda4beaf9cb22f814
Eryk Tyndel <tyndel.eryk@gmail.com>
2025-07-13 17:31:05 +0200

    feat: :sparkles: Dynamized header

--- header.php
+++ header.php
@@ -16,3 +16,45 @@
 
 <body class="--txt-light">
     <div class="side-wrapper">
+        <div class="header">
+            <?php
+
+            $header_loop = new WP_Query(array(
+                'post_type' => 'page-settings',
+                'order' => 'ASC',
+                'posts_per_page' => 1
+            ));
+
+            while ($header_loop->have_posts()):
+                $header_loop->the_post();
+            ?>
+                <div class="header__content">
+                    <div class="header__info">
+                        <div class="header__personal-data">
+                            <h1 class="heading--1 --is-bold"><?php the_field('name'); ?></h1>
+                            <h2 class="heading--2 --txt-grey"><?php the_field('position'); ?></h2>
+                        </div>
+
+                        <div class="header__links">
+                            <a class="header__link" href="<?php the_field("github_url"); ?>" target="_blank">
+                                <i class="fa-brands fa-square-github"></i>
+                            </a>
+                            <a class="header__link" href="<?php the_field("linkedin_url"); ?>" target="_blank">
+                                <i class="fa-brands fa-linkedin"></i>
+                            </a>
+                            <a class="header__link" href="maito:<?php the_field("email_address"); ?>" target="_blank"><i class="fa-solid fa-envelope"></i></a>
+                        </div>
+                    </div>
+
+                    <div class="header__photo-wrapper">
+                        <?php
+                        $image = get_field('photo');
+
+                        if (!empty($image)): ?>
+                            <img class="header__photo" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+                        <?php endif; ?>
+                    </div>
+                </div>
+            <?php endwhile; ?>
+            <?php wp_reset_postdata(); ?>
+        </div>
